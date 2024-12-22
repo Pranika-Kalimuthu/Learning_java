016775a5eaf68d29747295ec069a9dc7ac0e9652
Pranika Kalimuthu <123928079+Pranika-Kalimuthu@users.noreply.github.com>
2024-12-22 18:21:27 +0530

    Add files via upload

---
+++ VarDeclaration.java
@@ -0,0 +1,22 @@
+
+public class VarDeclaration {
+    public static void main(String[] args) {
+        int a=18;
+        String b="welcome";
+        float c=8.9f;
+        double d=2.333;
+        long e=2393301898687557l;
+        short f=2384;
+        boolean g=true;
+        byte h=34;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+   
+    }
+}
